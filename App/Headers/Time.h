c3387564dff427d8972b54366f1f9e48171f983e
Matheus Silva Freitas <msformigteen@live.com>
2017-08-19 23:28:18 -0300

    TRade BRanch

--- App/Headers/Time.h
+++ App/Headers/Time.h
@@ -4,4 +4,15 @@
 //Dependências
 #include <time.h>
 
+//Constante
+
+//Seta o Tempo ou Retorna o Valor do Tempo
+/*
+ * op = 0 - Read | 1 - Write
+ * type = 1 - List | 0 - Tree
+ * action = 00 - Busca Unica | 01 - Leitura Unica | 02 - Criação Única | 03 - Deleção Única | 12 - Importação
+ */
+void Time(int op, int type, int action, int value) {
+    static double
+}
 #endif
