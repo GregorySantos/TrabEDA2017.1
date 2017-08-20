d60e53150b9a9e80c9794c0432cb33fdded5de85
Matheus Silva Freitas <msformigteen@live.com>
2017-08-20 11:33:00 -0300

    Preparando Merge para Integração

--- App/Headers/Unissex.h
+++ App/Headers/Unissex.h
@@ -7,6 +7,7 @@
 
 void * Create(int type); //Cria alguma estrutura de Dado e retorna o seu ponteiro
 void Insert(void * Head, void * Node, int type ); //Insere em Lista se Type == 1, caso contrário, espera inserir em AVL
+void Delete(void *Tads[], int matricula, int type ); //Delta algum nó pela matricula
 
 //Insere em Lista se Type == 1, caso contrário, espera inserir em AVL
 void Insert(void * Head, void * Data, int type ) {
@@ -24,4 +25,12 @@ void * Create(int type) {
         //Return Função criar AVL
 }
 
+//Deleta algum nó pela Matricula
+void Delete(void *Tads[], int matricula, int type ) {
+    switch (type) {
+        case 1:
+            removeNode(Tads[1], int mat);
+            break;
+    }
+}
 #endif
