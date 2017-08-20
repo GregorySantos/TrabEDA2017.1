ca1dd7908ad32f76e0673b36ed6574de2ae63a93
Gregory Santos <gregory.sgsantos@gmail.com>
2017-08-20 00:10:08 -0300

    Função pra liberar ponteiros do datanode

--- App/Headers/Data.h
+++ App/Headers/Data.h
@@ -52,4 +52,14 @@ void setFileData( char * input, DataNode * new, int data ) {
 DataNode * createData() {
     return malloc(sizeof(DataNode));
 }
+
+//Libera todos os ponteiros de um datanode
+FreeDataNode(Datanode *D){
+    free(nome);
+    free(sobrenome);
+    free(email);
+    free(telefone);
+    free(D);
+}
+
 #endif
